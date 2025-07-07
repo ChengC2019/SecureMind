373ca1792f0025ab13f9a0008ae2cc97c46bc0ee
Cheng Chen <cheng.chen@uah.edu>
2025-07-07 19:10:31 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv
-
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
 load_dotenv()
 USER_AVATAR = "👤"
@@ -14,17 +18,71 @@ BOT_AVATAR = "🤖"
 # Show title and description.
 uah_logo_url = "https://www.uah.edu/images/administrative/communications/logo/uah-logo.svg"
 
+st.markdown("""
+    <style>
+        html, body, [class*="css"]  {
+            font-size: 18px !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
 # Create two columns: one for the logo, one for the title
 col1, col2 = st.columns([1, 6])  # Adjust ratio to size preference
 
 with col1:
     st.image(uah_logo_url, width=100)
 with col2:
-    st.title("Cyber Security Assistant")
-st.write(
-    "Please feel free to ask any questions about the cyber security related questions."
+    st.title("GenCyber Assistant")
+st.markdown(
+    "Please feel free to ask any questions about the cyber security related questions. "
     "Please ask and answer topic-related or factual questions. "
 )
+with st.sidebar:
+    st.subheader("Upload Cybersecurity Docs")
+    pdf_docs = st.file_uploader("Upload PDFs", accept_multiple_files=True)
+    if st.button("Process"):
+        with st.spinner("Processing..."):
+            raw_text = get_pdf_text(pdf_docs)
+            chunks = get_text_chunks(raw_text)
+            vectorstore = get_vectorstore(chunks)
+            st.session_state.conversation = get_conversation_chain(vectorstore)
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings(temperature=0.1)
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
@@ -44,7 +102,7 @@ else:
     # Display the existing chat messages via `st.chat_message`.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            st.markdown(message["content"], unsafe_allow_html=True)
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
@@ -53,7 +111,7 @@ else:
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
-            st.markdown(prompt)
+            st.markdown(prompt, unsafe_allow_html=True)
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
