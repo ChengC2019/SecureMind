7638b6018041296cf9ff1fe7223881bcc76007db
Cheng Chen <cheng.chen@uah.edu>
2025-07-07 20:57:10 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,16 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
+
 # from langchain.llms import HuggingFaceHub
 import os
 
-# load_dotenv()
-# USER_AVATAR = "👤"
-# BOT_AVATAR = "🤖"
+load_dotenv()
 
 # Show title and description.
 uah_logo_url = "https://www.uah.edu/images/administrative/communications/logo/uah-logo.svg"
@@ -40,71 +39,72 @@ st.markdown(
 )
 st.write(css, unsafe_allow_html=True)
 
-with st.sidebar:
-    st.subheader("Upload Cybersecurity Docs")
-    pdf_docs = st.file_uploader("Upload PDFs", accept_multiple_files=True)
-    if st.button("Process"):
-        with st.spinner("Processing..."):
-            raw_text = get_pdf_text(pdf_docs)
-            chunks = get_text_chunks(raw_text)
-            vectorstore = get_vectorstore(chunks)
-            st.session_state.conversation = get_message_chain(vectorstore)
+# with st.sidebar:
+#     st.subheader("Upload Cybersecurity Docs")
+#     pdf_docs = st.file_uploader("Upload PDFs", accept_multiple_files=True)
+#     if st.button("Process"):
+#         with st.spinner("Processing..."):
+#             raw_text = get_pdf_text(pdf_docs)
+#             chunks = get_text_chunks(raw_text)
+#             vectorstore = get_vectorstore(chunks)
+#             st.session_state.conversation = get_message_chain(vectorstore)
 
-def get_pdf_text():
-    text = ""
-    file_path = "./CSResourceReferenceGuide.pdf"
-    with open(file_path, "rb") as f:
-        pdf_reader = PdfReader(f)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+# def get_pdf_text():
+#     text = ""
+#     file_path = "./CSResourceReferenceGuide.pdf"
+#     with open(file_path, "rb") as f:
+#         pdf_reader = PdfReader(f)
+#         for page in pdf_reader.pages:
+#             text += page.extract_text()
+#     return text
 
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
+# def get_text_chunks(text):
+#     text_splitter = CharacterTextSplitter(
+#         separator="\n",
+#         chunk_size=1000,
+#         chunk_overlap=200,
+#         length_function=len
+#     )
+#     chunks = text_splitter.split_text(text)
+#     return chunks
 
-def get_vectorstore(text_chunks):
-    openai_key = openai_api_key
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+# def get_vectorstore(text_chunks):
+#     openai_key = openai_api_key
+#     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+#     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+#     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+#     return vectorstore
 
-def get_message_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
+# def get_message_chain(vectorstore):
+#     llm = ChatOpenAI()
+#     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+#     memory = ConversationBufferMemory(
+#         memory_key='chat_history', return_messages=True)
+#     conversation_chain = ConversationalRetrievalChain.from_llm(
+#         llm=llm,
+#         retriever=vectorstore.as_retriever(),
+#         memory=memory
+#     )
+#     return conversation_chain
 
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+# def handle_userinput(user_question):
+#     response = st.session_state.conversation({'question': user_question})
+#     st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+#     for i, message in enumerate(st.session_state.chat_history):
+#         if i % 2 == 0:
+#             st.write(user_template.replace(
+#                 "{{MSG}}", message.content), unsafe_allow_html=True)
+#         else:
+#             st.write(bot_template.replace(
+#                 "{{MSG}}", message.content), unsafe_allow_html=True)
             
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.secrets["openai"]["api_key"]
+
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
@@ -132,29 +132,40 @@ else:
         with st.chat_message("user"):
             st.markdown(prompt, unsafe_allow_html=True)
 
-        raw_text = get_pdf_text()
+        template = """
+        You are a cybersecurity assistant trained to analyze and explain technical content to middle school students. 
+        Your primary goal is to extract relevant information, answer user questions accurately, and respond with clear, concise explanations. 
 
-        # get the text chunks
-        text_chunks = get_text_chunks(raw_text)
+        Rules:
+        1. Only answer based on factual infomation.
+        2. If the question cannot be answered directly, say "I dont know the answers."
+        3. Use bullet points or numbered lists if helpful.
+        4. Always write in a professional but accessible tone for middle school students.
 
-        # create vector store
-        vectorstore = get_vectorstore(text_chunks)
-
-        # create conversation chain
-        st.session_state.conversation = get_conversation_chain(vectorstore)
-
+        If the document is technical, explain it as if the user is technically informed but not an expert.
+        CONTEXT: {scenario}
+        STORY:
+        """
+        prompt = PromptTemplate(template=template, input_variables=["scenario"])
+        
+        # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
         # Generate a response using the OpenAI API.
-        # stream = client.chat.completions.create(
-        #     model="gpt-3.5-turbo",
-        #     messages=[
-        #         {"role": m["role"], "content": m["content"]}
-        #         for m in st.session_state.messages
-        #     ],
-        #     stream=True,
-        # )
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        chain = LLMChain(llm=stream, prompt=prompt, verbose=True)
+        
+        story = chain.run(scenario=scenario)
+
+        
 
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
         with st.chat_message("assistant"):
-            response = st.write_stream(stream)
+            response = st.write_stream(story)
         st.session_state.messages.append({"role": "assistant", "content": response})
